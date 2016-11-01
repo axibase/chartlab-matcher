d65ed47824ea32add234bae36f7211b7eea073c0
VeselovAlex <veselov143@gmail.com>
2016-11-01 12:41:01 +0300

    Broken test fixed

--- src/matcher/src/test/java/com/axibase/chartstesting/screenshotmatcher/core/PortalTest.java
+++ src/matcher/src/test/java/com/axibase/chartstesting/screenshotmatcher/core/PortalTest.java
@@ -10,8 +10,8 @@ import static org.junit.Assert.*;
 public class PortalTest {
 
     @Test
-    public void testParseURL() throws Exception {
-        Portal p = new Portal("https://example.com/chartlab/075941a0/15");
+    public void testParsePath() throws Exception {
+        Portal p = new Portal("075941a0/15");
         assertEquals("correct id", "075941a0", p.getConfigId());
         assertEquals("correct revision", "15", p.getRevisionString());
     }
