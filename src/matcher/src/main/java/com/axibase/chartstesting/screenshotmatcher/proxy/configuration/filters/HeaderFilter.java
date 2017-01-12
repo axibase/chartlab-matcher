40fc8e176e97e8c7c9af89bb065115bd1fe76972
Aleksandr Veselov <veselov143@gmail.com>
2017-01-12 17:12:40 +0300

    fix of comma separation

--- src/matcher/src/main/java/com/axibase/chartstesting/screenshotmatcher/proxy/configuration/filters/HeaderFilter.java
+++ src/matcher/src/main/java/com/axibase/chartstesting/screenshotmatcher/proxy/configuration/filters/HeaderFilter.java
@@ -14,30 +14,30 @@ public class HeaderFilter implements LineFilter {
             "deeppink, dodgerblue, greenyellow, green, lightgreen, lightsalmon, " + 
             "mediumaquamarine, midnightblue, olivedrab, red, saddlebrown, " +
             "skyblue, slategrey, tomato, yellow, yellowgreen, steelblue, khaki, " + 
-            "coral, brown, palegoldenrod, lightblue, pink, paleturquoise" +
-            "darkseagreen, powderblue, deepskyblue, azure, lightslategray" +
-            "lightgray, royalblue, mediumvioletred, darkmagenta, maroon" +
-            "goldenrod, mediumturquoise, lightyellow, lemonchiffon" +
+            "coral, brown, palegoldenrod, lightblue, pink, paleturquoise, " +
+            "darkseagreen, powderblue, deepskyblue, azure, lightslategray, " +
+            "lightgray, royalblue, mediumvioletred, darkmagenta, maroon, " +
+            "goldenrod, mediumturquoise, lightyellow, lemonchiffon, " +
             "lavenderblush, tan, purple, darkkhaki, springgreen, moccasin, " + 
-            "mintcream, salmon, beige, papayawhip, grey, lightsteelblue" +
-            "silver, chocolate, firebrick, indigo, indianred, darkolivegreen" +
-            "slateblue, darkgreen, navajowhite, dimgray, violet, cadetblue, oldlace" +
-            "orchid, lightgoldenrodyellow, snow, floralwhite, wheat, sandybrown" +
-            "teal, thistle, blanchedalmond, ghostwhite, hotpink, cornsilk" +
-            "cornflowerblue, mistyrose, gold, olive, forestgreen" +
-            "orangered, slategray, lightslategrey" +
-            "cyan, mediumspringgreen, darkgrey, darkorchid, linen, turquoise" +
-            "seashell, rosybrown, peachpuff, mediumslateblue" +
-            "honeydew, darkturquoise" +
-            "whitesmoke, lavender, lightgrey, magenta, bisque, lime" +
-            "mediumblue, lightcoral, mediumpurple" +
-            "rebeccapurple, limegreen, sienna, lawngreen" +
-            "darkviolet, navy, darkslategrey, aqua" +
-            "mediumseagreen, darkgray, plum, black, seagreen" +
-            "lightseagreen, antiquewhite, orange, lightskyblue" +
-            "peru, dimgrey, lightcyan, darkslategray" +
-            "palegreen, lightpink, ivory" +
-            "fuchsia, mediumorchid, gray, palevioletred, darkred" +
+            "mintcream, salmon, beige, papayawhip, grey, lightsteelblue, " +
+            "silver, chocolate, firebrick, indigo, indianred, darkolivegreen, " +
+            "slateblue, darkgreen, navajowhite, dimgray, violet, cadetblue, oldlace, " +
+            "orchid, lightgoldenrodyellow, snow, floralwhite, wheat, sandybrown, " +
+            "teal, thistle, blanchedalmond, ghostwhite, hotpink, cornsilk, " +
+            "cornflowerblue, mistyrose, gold, olive, forestgreen, " +
+            "orangered, slategray, lightslategrey, " +
+            "cyan, mediumspringgreen, darkgrey, darkorchid, linen, turquoise, " +
+            "seashell, rosybrown, peachpuff, mediumslateblue, " +
+            "honeydew, darkturquoise, " +
+            "whitesmoke, lavender, lightgrey, magenta, bisque, lime, " +
+            "mediumblue, lightcoral, mediumpurple, " +
+            "rebeccapurple, limegreen, sienna, lawngreen, " +
+            "darkviolet, navy, darkslategrey, aqua, " +
+            "mediumseagreen, darkgray, plum, black, seagreen, " +
+            "lightseagreen, antiquewhite, orange, lightskyblue, " +
+            "peru, dimgrey, lightcyan, darkslategray, " +
+            "palegreen, lightpink, ivory, " +
+            "fuchsia, mediumorchid, gray, palevioletred, darkred, " +
             "white, blue, aliceblue, darksalmon, gainsboro";
 
         if (portal.hasEndtime()) {
